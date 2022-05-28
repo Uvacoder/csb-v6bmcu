3b174528c21484a3246695b019dbdf6524aeef9a
Code Sandbox <hello@codesandbox.io>
2022-05-28 00:25:34 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.ac464207c8b98536ebca8f13bc941ce3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "439e1403d3fabe647ffbd8e6bdb878c1",
+    "url": "https://uvacoder.github.io/csb-v6bmcu/index.html"
+  },
+  {
+    "revision": "a4956c659d50c66239c5",
+    "url": "https://uvacoder.github.io/csb-v6bmcu/static/js/2.feb37266.chunk.js"
+  },
+  {
+    "revision": "f0ad72fb0c1c1e4a268423d8869404c9",
+    "url": "https://uvacoder.github.io/csb-v6bmcu/static/js/2.feb37266.chunk.js.LICENSE"
+  },
+  {
+    "revision": "97d234b8bbe3b45c1fa8",
+    "url": "https://uvacoder.github.io/csb-v6bmcu/static/js/main.af44f0e0.chunk.js"
+  },
+  {
+    "revision": "54414253c458fac9f0fd",
+    "url": "https://uvacoder.github.io/csb-v6bmcu/static/js/runtime-main.0c5ba854.js"
+  }
+]);
